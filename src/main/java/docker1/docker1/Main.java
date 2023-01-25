84080c90713c10aefbce29eea742f349edbe3fe7
Group015 <Group015@infoway.com>
2023-01-25 11:05:03 +0530

    Addes java demo application files1

---
+++ src/main/java/docker1/docker1/Main.java
@@ -0,0 +1,18 @@
+package docker1.docker1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println(checkIfInputIsAnEvenNumber(122));
+    }
+    public static boolean checkIfInputIsAnEvenNumber(int number)
+    {
+    return number % 2 == 0;
+    }
+    		
+}
